abae45802679d63c5f1d71ff4d90300e18a53482
garethbufton76-blip <garethbufton76@hotmail.com>
2026-05-06 13:05:33 +0100

    Create Score.tsx

---
+++ src/components/Score.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Score() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-bold">Score Screen</h2>
+      <p>This will replace your giant scoring block</p>
+    </div>
+  );
+}
