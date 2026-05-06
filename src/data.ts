fa7904e8fd36ff7263447070ea4cce082ddb85d9
garethbufton76-blip <garethbufton76@hotmail.com>
2026-05-06 12:59:26 +0100

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,90 +1,54 @@
-export type TeamKey = "Red" | "Blue";
-export type Side = "red" | "blue";
+export const STORAGE_KEY = "dual-in-the-dunes-v1";
 
-export const gold = "#d1c79f";
-
-export const TEAM = {
-  red: { key: "Red", label: "R", colour: "#8b1f2d" },
-  blue: { key: "Blue", label: "B", colour: "#214aa8" },
-};
+export const playerOptions = [1, 2, 4, 6, 8, 10, 12];
+export const dayOptions = [1, 2, 3, 4];
 
 export const formats = [
   "Singles Match Play",
   "2-Ball Better Ball",
+  "4 Player Stableford",
+  "Foursomes",
   "2-Ball Ambrose",
   "Stableford",
-  "Foursomes",
-  "Greensomes",
-  "Chapman",
   "Par / Bogey",
+  "Chapman (Pinehurst)",
+  "Greensomes",
 ];
 
-export const tees = ["Blue", "White", "Gold", "Red"];
-
-export const defaultDay = {
-  label: "Day 1",
-  course: "St Michaels",
-  tee: "Blue",
-  format: "Singles Match Play",
+export const minPlayers: Record<string, number> = {
+  "Singles Match Play": 1,
+  Stableford: 1,
+  "Par / Bogey": 1,
+  "2-Ball Better Ball": 2,
+  "2-Ball Ambrose": 2,
+  Foursomes: 2,
+  "Chapman (Pinehurst)": 2,
+  Greensomes: 2,
+  "4 Player Stableford": 4,
 };
 
-export const stMichaelsRows = [
-  [1,399,1,4,378,2,4,351,6,4,378,17,5],
-  [2,317,4,4,303,4,4,290,8,4,291,1,4],
-  [3,170,10,3,166,12,3,130,16,3,133,12,3],
-  [4,346,8,4,291,16,4,274,12,4,272,16,4],
-  [5,203,6,3,170,10,3,129,18,3,150,9,3],
-  [6,501,16,5,433,18,5,425,14,5,425,14,5],
-  [7,492,14,5,472,8,5,441,2,5,446,3,5],
-  [8,398,12,4,390,6,4,378,4,4,398,18,5],
-  [9,298,18,4,287,14,4,263,10,4,275,7,4],
-  [10,375,7,4,368,1,4,305,13,4,307,8,4],
-  [11,373,3,4,339,5,4,325,5,4,328,2,4],
-  [12,176,13,3,155,17,3,135,15,3,137,11,3],
-  [13,472,15,5,457,11,5,385,1,4,385,13,5],
-  [14,340,17,4,333,15,4,324,7,4,283,10,4],
-  [15,206,11,3,200,13,3,171,17,3,179,15,3],
-  [16,379,5,4,352,9,4,330,11,4,333,4,4],
-  [17,498,2,5,417,7,5,402,3,4,403,5,5],
-  [18,402,9,4,375,3,4,321,9,4,324,6,4],
-];
+export const times = ["7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00"];
+export const tees = ["Blue", "White", "Gold", "Red"];
 
-export const holesByTee: any = Object.fromEntries(
-  stMichaelsRows.map(([hole, ...v]) => [
-    hole,
-    Object.fromEntries(
-      tees.map((tee, i) => {
-        const [metres, si, par] = v.slice(i * 3, i * 3 + 3);
-        return [tee, { hole, metres, si, par }];
-      })
-    ),
-  ])
-);
+export const TEAM = {
+  red: {
+    label: "R",
+    title: "Team Red",
+    grad: "from-[#9e2535] via-[#6f1725] to-[#2b080f]",
+    bg: "from-[#381018] via-[#101010] to-black",
+  },
+  blue: {
+    label: "B",
+    title: "Team Blue",
+    grad: "from-[#244fb4] via-[#132a70] to-[#07102c]",
+    bg: "from-[#10224e] via-[#101010] to-black",
+  },
+};
 
-export function makeRoster() {
-  const base = [
-    ["Gareth Bufton", 4],
-    ["Mark McLeod", 7],
-    ["Nick Gerard", 10],
-    ["Jimmy Neale", 13],
-    ["Hayden Abercrombie", 16],
-    ["Areef Vohra", 19],
-  ];
-
-  return {
-    Red: base.map(([name, handicap]) => ({ name, handicap, photo: "" })),
-    Blue: base.map(([name, handicap]) => ({ name, handicap, photo: "" })),
-  };
-}
-
-export const defaultState = {
-  players: 2,
-  day: defaultDay,
-  teamNames: { Red: "LEGENDS", Blue: "FURY" },
-  teamLogos: { Red: "", Blue: "" },
-  roster: makeRoster(),
-  holes: Array.from({ length: 18 }, (_, i) => ({
-    hole: i + 1,
-    status: "pending",
-  })),
+export const BACKGROUND_IMAGES = {
+  home: "https://i.ibb.co/B5MCPFwV/hf-20260406-212338-4e6f71fe-a63d-4837-9341-31237b0552c3.png",
+  rosterP: "",
+  rosterB: "",
+  score: "",
+  admin: "",
 };
